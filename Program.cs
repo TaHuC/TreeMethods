7c1e21ee69503887279373b911c942d8dd6c05b5
TaHuC <TaHuC@hotmail.com>
2017-01-29 22:11:38 +0200

    Finish Blank Receipt

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TreeMethods
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Header();
+			Body();
+			Footer();
+		}
+
+		public static void Header()
+		{
+			Console.WriteLine("CASH RECEIPT");
+			Console.WriteLine("------------------------------");
+		}
+
+		public static void Body()
+		{
+			Console.WriteLine("Charged to____________________");
+			Console.WriteLine("Received by___________________");
+		}
+
+		public static void Footer()
+		{
+			Console.WriteLine("------------------------------");
+			Console.WriteLine("© SoftUni");
+		}
+	}
+}
